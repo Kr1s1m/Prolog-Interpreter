828014f5a35d707277a4208f17cb96d7d3c6f505
Kristian Simov <krisi.simov@gmail.com>
2022-01-16 04:16:09 +0200

    Added Knowledgebase module. Contains Knowledgebase functionality.

--- src/Knowledgebase.hs
+++ src/Knowledgebase.hs
@@ -1,5 +1,6 @@
 module Knowledgebase where
-import Types
+    
+import Types( KnowledgeBase, Program, Clause(head), Term(funct), Funct )
 import Data.Map.Lazy as Map (empty, insertWith, lookup)
 import Data.Maybe(fromMaybe)
 
