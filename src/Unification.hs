203fc1e9cd2412148b91d52de90b758620703798
Kristian Simov <krisi.simov@gmail.com>
2022-01-29 14:25:21 +0200

    Unification module - simplified unification and added apply substitution methods.

--- src/Unification.hs
+++ src/Unification.hs
@@ -1,32 +1,34 @@
 module Unification where
 
-import Types ( Term(Var, Const, Comp), Substitutions )
-
-import Data.List(zipWith)
+import Types ( Term(Var, Const, Comp), Substitution, true )
 
 
-unify :: Term -> Term -> Substitutions
-unify (Const c1) (Const c2) = if c1 == c2 then Just [] else Nothing
-unify (Var v) (Const c) = Just [(Var v, Const c)]
-unify (Const c) (Var v) = Just [(Var v, Const c)]
-unify (Var v1) (Var v2) = if v1 == v2 then Just [] else Just [(Var v1, Var v2), (Var v2, Var v1)]
-unify (Var v) (Comp f args) = Just [(Var v, Comp f args)]
-unify (Comp f args) (Var v)  = Just [(Var v, Comp f args)]
-unify (Comp f1 args1) (Comp f2 args2) = if f1 == f2 then checkForDupVar $ joinUnified (zipWith unify args1 args2) [] else Nothing   
-    where 
-          joinUnified [] res = Just res
-          joinUnified (u:us) res = case u of
-              Just [] -> joinUnified us res
-              Just sub -> joinUnified us (sub ++ res)
-              Nothing -> Nothing
-
-          checkForDupVar joined = case joined of
-              Nothing -> Nothing 
-              Just j -> if any (\(x, y) -> isVar x && not ( isVar y ) && any (\(z, t) -> z == x && not ( isVar t) && y /= t) j )  j
-                        then Nothing 
-                        else Just j
- 
-          isVar (Var _) = True
-          isVar _ = False
-
+unify :: Term -> Term -> Maybe Substitution
+unify (Const c1) (Const c2) = if c1 == c2 then Just true else Nothing
+unify (Var v1) (Var v2)     = if v1 == v2 then Just true else Just [(Var v1, Var v2)]
+unify (Var v) t = Just [(Var v, t)] 
+unify t (Var v) = Just [(Var v, t)]
+unify (Comp f1 args1) (Comp f2 args2) = if f1 == f2 then unify' args1 args2 else Nothing
 unify _ _ = Nothing
+
+
+unify' :: [Term] -> [Term] -> Maybe Substitution
+unify' [] [] = Just true
+unify' _ [] = Nothing
+unify' [] _ = Nothing
+unify' (t:ts) (u:us) = do
+    s  <- unify t u
+    s' <- unify' (applySubstitution s ts) (applySubstitution s us)
+    return (s ++ s')
+
+
+applySubstitution :: Substitution -> [Term] -> [Term]
+applySubstitution s = map (applySubstitution' s)
+
+applySubstitution' :: Substitution -> Term -> Term
+applySubstitution' [] t = t
+applySubstitution' ((Var v1, t):ss) (Var v2)
+    | v1 == v2  = applySubstitution' ss t
+    | otherwise = applySubstitution' ss (Var v2)
+applySubstitution' s (Comp f args) = Comp f $ applySubstitution s args
+applySubstitution' _ t = t
