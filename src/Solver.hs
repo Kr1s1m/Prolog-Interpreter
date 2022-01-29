22736300357f2f387c5726f2a37e973a32c7a7ed
Kristian Simov <krisi.simov@gmail.com>
2022-01-29 14:19:33 +0200

    Added Solver module containing resolution

---
+++ src/Solver.hs
@@ -0,0 +1,49 @@
+module Solver where
+
+import Types
+    ( true,
+      Program,
+      Substitution,
+      Term(Comp, Const, Var),
+      Goals,
+      Rule(Rule),
+      Branch,
+      getGoals, showSubstitution )
+
+import Unification ( unify, applySubstitution )
+import Data.Maybe (maybeToList)
+import Parser ( parseCommand, parseProgram )
+
+
+solve :: Program -> Goals -> [Substitution]
+solve = dfs 1
+
+dfs :: Int -> Program -> Goals -> [Substitution]
+dfs _ _ [] = [true]
+dfs n program goals = do
+    (s, goals') <- branch (changeNamespace n program) goals
+    solution    <- dfs (n + 1) program goals'
+    return (s ++ solution)
+
+branch :: Program -> Goals -> Branch
+branch [] _ = []
+branch _ [] = []
+branch program (goal:goals) = do
+    r@(Rule head body) <- program
+    s                  <- maybeToList (unify goal head)
+    return (s, applySubstitution s (body ++ goals))
+
+changeNamespace :: Int -> Program -> Program
+changeNamespace n = map renameRule
+    where
+
+        renameRule :: Rule -> Rule
+        renameRule (Rule head body) = Rule (renameTerm head) (renameTerms body)
+
+        renameTerm :: Term -> Term
+        renameTerm c@(Const _)      = c
+        renameTerm (Var (s, _))     = Var (s, n)
+        renameTerm (Comp f args)    = Comp f (renameTerms args)
+
+        renameTerms :: [Term] -> [Term]
+        renameTerms = map renameTerm
