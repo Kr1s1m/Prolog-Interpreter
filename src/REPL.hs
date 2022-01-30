03fdd09fdac797a70497934f6ea2a3a51d7f9087
Kristian Simov <krisi.simov@gmail.com>
2022-01-30 22:37:21 +0200

    REPL module - cleaned imports and customized user messages

--- src/REPL.hs
+++ src/REPL.hs
@@ -1,17 +1,12 @@
 module REPL where
 
-import Types
-    ( Command(..), Substitution, Program, Term, showSubstitution )
+import Types( Command(..), Substitution, Program, Term, showSubstitution )
 import Parser ( parseCommand, parseProgram )
 import Unification ( unify', applySubstitution )
 import Solver ( solve )
+
 import Data.Maybe ( fromMaybe )
-import Control.Monad (when)
-
-
 import System.IO ( IOMode(ReadMode), hGetContents, openFile, hClose)
-import Control.DeepSeq ( deepseq )
-import GHC.Conc (pseq)
 
 prologREPL :: IO()
 prologREPL = do
@@ -25,14 +20,15 @@ prologREPL = do
 
     if null program
     then
-        putStrLn "Syntax error inside .pl file!!!"
-    else 
+        putStrLn "Syntax error found inside .pl file.\nPlease fix errors and reload."
+    else do
+        putStrLn "File loaded and intepreted. No errors found.\n" 
         interactLine (initializeREPL program)
 
     hClose handle
 
 --TODO: Causes *** Exception: <stdin>: hGetLine: illegal operation (handle is semi-closed)
---on next load of main function which requires reload of Main.hs to execute main again
+--on next load of main function which requires reload of Main module to execute main again
 interactLine :: (String -> String) -> IO ()
 interactLine f = loop
   where
@@ -44,7 +40,7 @@ initializeREPL :: Program -> (String -> String)
 initializeREPL program = writeStr welcomeMsg (repl program)
 
 repl ::  Program -> (String -> String)
-repl program = writeStr "Haskellog*> " $ readLine (execute program . parseCommand)
+repl program = writeStr "*Haskellog> " $ readLine (execute program . parseCommand)
 
 execute :: Program -> Command -> (String -> String)
 execute program (AddRule rule)      = writeStr ("\nRule added: " ++ show rule ++ "\n") (repl (program ++ [rule]))
@@ -54,7 +50,6 @@ execute program Help                = writeStr helpMsg (repl program)
 execute program NoAction            = repl program
 execute program CmdError            = writeStr errorMsg (repl program)
 execute program Quit                = end
-
 
 getSolution :: [Term] -> [Substitution] -> Program -> (String -> String)
 getSolution q [] program     = writeStr "false.\n" (repl program)
@@ -70,8 +65,8 @@ getAllRules :: Program -> String
 getAllRules = concatMap (\r -> show r ++ "\n")
 
 welcomeMsg :: String
-welcomeMsg = "Welcome to Prolog in Haskell version 2.1!\n" ++
-             "For help, please enter :help\n"
+welcomeMsg = "****Welcome to Prolog in Haskell (Haskellog) version 2.2.1!****\n" ++
+             "****For help, please enter :help\n"
              
 end :: String -> String
 end _ = "Interaction ended by user. Reload main to initiate it again.\n"
@@ -88,12 +83,12 @@ helpMsg =
 errorMsg :: String
 errorMsg = "Unknown command! Enter :help in prompt for more information.\n"
 
--- Its arguments are a string to be written and next process p.
-writeStr :: String -> (String -> String) -> (String -> String)
+-- writeStr arguments are a string to be written and next process p.
+writeStr :: String -> (String -> String) -> String -> String
 writeStr output p input = output ++ p input
 
--- Its argument is a process p which receives a line.
-readLine :: (String -> (String -> String)) -> (String -> String)
+-- readLine argument is a process p which receives a line.
+readLine :: (String -> String -> String) -> String -> String
 readLine p input = case nextLine input of
                         ("", [])      -> "" -- End of file
                         (line, rest) -> p line rest
